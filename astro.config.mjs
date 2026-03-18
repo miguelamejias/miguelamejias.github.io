e2f22ab6fdcfe0c9a79dba438b63d3a1af7f6708
Miguel Angel Mejia Sanchez <75095785+miguelamejias@users.noreply.github.com>
2026-03-18 15:08:59 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/miguelamejias/miguelamejias.github.io",
+  site: "https://miguelamejias.github.io",
   integrations: [icon(), sitemap()],
 });
